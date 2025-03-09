6d8eab723e44eb0bb43b8c4a7d206dcc3c7ebb43
JaniShesh <jasheshan011@gmail.com>
2025-03-10 02:49:36 +0530

    change images

--- forms/contact.php
+++ forms/contact.php
@@ -10,12 +10,15 @@
   $receiving_email_address = 'contact@example.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
+    include $php_email_form;
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
   }
 
-  $contact = new PHP_Email_Form;
+  if (!class_exists('PHP_Email_Form')) {
+    die('PHP_Email_Form class not found!');
+  }
+  $contact = new PHP_Email_Form();
   $contact->ajax = true;
   
   $contact->to = $receiving_email_address;
@@ -35,7 +38,6 @@
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
 ?>
